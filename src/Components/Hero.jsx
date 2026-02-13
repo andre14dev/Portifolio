1d7d874aecc9dd02b7e076f2f0a471ad281566ae
andre14dev <andre.mota@ges.inatel.br>
2026-02-12 22:00:06 -0300

    commit de atualização do Hero.jsx e dependências

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,14 @@
-import { Laptop  } from 'lucide-react';
+import Spline from '@splinetool/react-spline';
+import { Laptop, Github, Linkedin } from 'lucide-react';
 
 
 const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
       
-        <div className="max-w-xl ml-[5%] z-10 mt-[100%] md:mt-[60%] lg:mt-0">   
-            <div className='relative w-fit sm:w-65 h-15 bg-gradient-to-r bg-gradient-to-r from-[#1E3A8A] to-[#2563EB]
-             shadow-[0_0_10px_rgba(255,255,255,0.4)] rounded-full '> 
+        <div className="hmax-w-xl ml-[5%] z-10 mt-[100%] md:mt-[60%] lg:mt-0">   
+            <div className='bg-gradient-to-r w-fit from-[#1E3A8A] to-[#2563EB] text-white h-9 px-6 py-3 
+            rounded-full font-medium hover:opacity-90 transition-opacity duration-300 flex items-center'> 
                <div className="rounded-full flex items-center whitespace-nowrap px-5">
                     <Laptop  className="w-7 h-7 mr-2" />
                     <span className="text-slate-100 font-medium relative -top-0.5 text-[#0F172A]">Desenvolvedor de software</span>
@@ -20,6 +21,8 @@ const Hero = () => {
             André Luiz <br /> 
         </h1>
 
+        <div className='flex flex-col gap-7'>
+
         <p className='text-base sm:text-lg tracking-wider text-gray-400 max-w-[30rem]'>
             Olá, me chamo André Luiz. Estou desenvolvendo minha carreira para me tornar 
             desenvolvedor de software e venho construindo minha base com dedicação e projetos reais. 
@@ -28,13 +31,23 @@ const Hero = () => {
             pode resolver problemas reais, e estando inserido no ambiente de constante aprendizado.
         </p>
 
-        <div>
-            <a href='https://github.com/andre14dev' className="bg-gradient-to-r from-[#1E3A8A] to-[#2563EB] text-white px-6 py-3 rounded-full font-medium hover:opacity-90 transition-opacity duration-300">
-                GitHub
+        <div className='flex gap-4'>
+            <a href='https://github.com/andre14dev' className="bg-gradient-to-r w-fit from-[#1E3A8A] to-[#2563EB] text-white
+             px-6 py-3 rounded-full font-medium flex items-center transition-all duration-300 hover:bg-white hover:bg-none hover:text-[#2563EB]">
+                <Github className="w-5 h-5 mr-2 stroke-current" /> GitHub
             </a>
-        </div>
+
+            <a href='https://www.linkedin.com/in/andreluiz04/' className="bg-gradient-to-r w-fit from-[#1E3A8A] to-[#2563EB] text-white
+             px-6 py-3 rounded-full font-medium flex items-center transition-all duration-300 hover:bg-white hover:bg-none hover:text-[#2563EB]">
+                <Linkedin className="w-5 h-5 mr-2 stroke-current" /> LinkedIn
+            </a>
 
         </div>
+        </div>
+        </div>
+        
+        <Spline className='absolute lg:top-0 top-[-20] bottom-0 lg:left-[-2%] h-full' scene="https://prod.spline.design/yOoQnggziGoRb62Z/scene.splinecode" />
+
 
     </main>
   
