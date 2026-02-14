4f191fee132b95d89195de0bc5fb501e9d68720d
andre14dev <andre.mota@ges.inatel.br>
2026-02-13 22:50:53 -0300

    Atualização do fundo da seção Hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main>
       {/* Gradient */}
-      <img className="absolute top-0 right-0 opacity  -60 -z-10" src="/gradient.png" alt="Gradient-img" />
+      <img className="absolute top-0 right-0 opacity-60 -z-10" src="/gradient.png" alt="Gradient-img" />
 
       {/* blur effect */}
       <div className="h-0 w-[45rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#2563EB]
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,19 +1,18 @@
-import React, { Suspense, lazy } from 'react';
 import { Laptop, Github, Linkedin } from 'lucide-react';
 
-
-const Spline = lazy(() => import('@splinetool/react-spline'));
 
 const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
       
-        <div className="hmax-w-xl ml-[5%] z-10 mt-[100%] md:mt-[60%] lg:mt-0">   
+        <img className="absolute left-1/2 -translate-x-1/2 lg:left-auto lg:right-0 lg:translate-x-0 top-0 -z-10 opacity-35 " src="/fundo.png" />
+
+        <div className="max-w-2xl ml-[5%] z-10 mt-[100%] md:mt-[60%] lg:mt-0">   
             <div className='bg-gradient-to-r w-fit from-[#1E3A8A] to-[#2563EB] text-white h-9 px-6 py-3 
             rounded-full font-medium hover:opacity-90 transition-opacity duration-300 flex items-center'> 
                <div className="rounded-full flex items-center whitespace-nowrap px-5">
                     <Laptop  className="w-7 h-7 mr-2" />
-                    <span className="text-slate-100 font-medium relative -top-0.5 text-[#0F172A]">Desenvolvedor de software</span>
+                    <span className="text-slate-100 font-medium relative -top-0.5">Desenvolvedor de software</span>
                 </div>
              </div>
         
@@ -25,22 +24,18 @@ const Hero = () => {
 
         <div className='flex flex-col gap-7'>
 
-        <p className='text-base sm:text-lg tracking-wider text-gray-400 max-w-[30rem]'>
-            Olá, me chamo André Luiz. Estou desenvolvendo minha carreira para me tornar 
-            desenvolvedor de software e venho construindo minha base com dedicação e projetos reais. 
-            Atualmente, curso Engenharia de Software pelo INATEL, onde venho 
-            ampliando meus conhecimentos técnicos e minha visão sobre como a tecnologia 
-            pode resolver problemas reais, e estando inserido no ambiente de constante aprendizado.
+        <p className='text-white text-base sm:text-lg tracking-wider text-gray-400 max-w-[42rem]'>
+            Olá, me chamo André Luiz. Estou desenvolvendo minha carreira para me tornar desenvolvedor de software e venho construindo minha base com dedicação e projetos reais. Atualmente, curso Engenharia de Software pelo INATEL, onde venho ampliando meus conhecimentos técnicos e minha visão sobre como a tecnologia pode resolver problemas reais, e estando inserido no ambiente de constante aprendizado.
         </p>
 
         <div className='flex gap-4'>
             <a href='https://github.com/andre14dev' className="bg-gradient-to-r w-fit from-[#1E3A8A] to-[#2563EB] text-white
-             px-6 py-3 rounded-full font-medium flex items-center transition-all duration-300 hover:bg-white hover:bg-none hover:text-[#2563EB]">
+             px-6 py-3 rounded-full font-medium flex items-center transition-all duration-300 hover:bg-white hover:text-[#2563EB]">
                 <Github className="w-5 h-5 mr-2 stroke-current" /> GitHub
             </a>
 
             <a href='https://www.linkedin.com/in/andreluiz04/' className="bg-gradient-to-r w-fit from-[#1E3A8A] to-[#2563EB] text-white
-             px-6 py-3 rounded-full font-medium flex items-center transition-all duration-300 hover:bg-white hover:bg-none hover:text-[#2563EB]">
+             px-6 py-3 rounded-full font-medium flex items-center transition-all duration-300 hover:bg-white hover:text-[#2563EB]">
                 <Linkedin className="w-5 h-5 mr-2 stroke-current" /> LinkedIn
             </a>
 
@@ -48,9 +43,7 @@ const Hero = () => {
         </div>
         </div>
         
-        <Suspense fallback={<div className="hidden lg:block h-[500px]"></div>}>
-          <Spline className='w-[300px] h-[300px] hidden lg:block absolute right-0 top-0 w-[600px] h-[600px] lg:translate-x-[200px]' scene=" https://prod.spline.design/tEmpMZm5eJXw4zc2/scene.splinecode" />
-        </Suspense>
+        
 
 
     </main>
